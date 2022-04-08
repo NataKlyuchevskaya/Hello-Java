5e7616ead943c8abfe0bd1e96e5f3cd9e3264610
Nata <nata.klyuchevskaya@gmail.com>
2022-04-08 09:10:34 +0300

    test Hello Java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package geekbrains.lesson_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("I will study Java!");
+    }
+}
